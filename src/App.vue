838619cd938f1aa6ec5478e67c08593aa434ca13
Kushal Jain <jain.kushal89@gmail.com>
2018-07-13 18:55:40 +0530

    ui for the components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+        <img src="./assets/logo.png">
         <router-view/>
         <Modal></Modal>
         <Loader></Loader>
@@ -19,4 +20,13 @@
     }
 </script>
 
-<style></style>
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
